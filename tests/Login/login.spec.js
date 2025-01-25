bea9a739b17cd4243385afc2c7c2a0af770cde18
lisandracabe <lisandracabe2@gmail.com>
2025-01-24 21:27:45 -0300

    Subindo projeto para o desafio

---
+++ tests/Login/login.spec.js
@@ -0,0 +1,43 @@
+// @ts-check
+import { test } from '@playwright/test';
+import {
+  dadoQueEstouNaPaginaDeLogin,
+  quandoEuInsiroOEmail,
+  quandoEuInsiroASenha,
+  quandoEuClicoNoBotao,
+  entaoDevoVerOLink,
+  entaoDevoVerAMensagem,
+} from './loginHelper';
+
+test.describe('Testes de Login', () => {
+  
+  test('Dado que estou na página de login, Quando insiro email e senha válidos, Então devo ver o link Sair', async ({ page }) => {
+    await dadoQueEstouNaPaginaDeLogin(page);
+    await quandoEuInsiroOEmail(page, 'qa@adset.com.br');
+    await quandoEuInsiroASenha(page, '9PK6#E8m');
+    await quandoEuClicoNoBotao(page, ' Entrar');
+    await entaoDevoVerOLink(page, ' Sair');
+  });
+
+  test('Dado que estou na página de login, Quando insiro apenas o email, Então devo ver a mensagem para preencher a senha', async ({ page }) => {
+    await dadoQueEstouNaPaginaDeLogin(page);
+    await quandoEuInsiroOEmail(page, 'qa@adset.com.br');
+    await quandoEuClicoNoBotao(page, ' Entrar');
+    await entaoDevoVerAMensagem(page, 'Por favor, digite a sua senha.');
+  });
+
+  test('Dado que estou na página de login, Quando insiro apenas a senha, Então devo ver a mensagem para preencher o email', async ({ page }) => {
+    await dadoQueEstouNaPaginaDeLogin(page);
+    await quandoEuInsiroASenha(page, '9PK6#E8m');
+    await quandoEuClicoNoBotao(page, ' Entrar');
+    await entaoDevoVerAMensagem(page, 'Por favor, digite o seu e-mail.');
+  });
+
+  test('Dado que estou na página de login, Quando não insiro email e senha, Então devo ver mensagens para preencher ambos', async ({ page }) => {
+    await dadoQueEstouNaPaginaDeLogin(page);
+    await quandoEuClicoNoBotao(page, ' Entrar');
+    await entaoDevoVerAMensagem(page, 'Por favor, digite o seu e-mail.');
+    await entaoDevoVerAMensagem(page, 'Por favor, digite a sua senha.');
+  });
+
+});
---
+++ tests/Login/loginHelper.js
@@ -0,0 +1,26 @@
+// loginHelper.js
+import { test, expect } from '@playwright/test';
+
+export async function dadoQueEstouNaPaginaDeLogin(page) {
+    await page.goto('https://www.adset.com.br/integrador');
+  }
+  
+  export async function quandoEuInsiroOEmail(page, email) {
+    await page.getByRole('textbox', { name: 'E-mail: ' }).fill(email);
+  }
+  
+  export async function quandoEuInsiroASenha(page, senha) {
+    await page.locator('#Senha').fill(senha);
+  }
+  
+  export async function quandoEuClicoNoBotao(page, botao) {
+    await page.getByRole('button', { name: botao }).click();
+  }
+  
+  export async function entaoDevoVerOLink(page, link) {
+    await expect(page.getByRole('link', { name: link })).toBeVisible();
+  }
+  
+  export async function entaoDevoVerAMensagem(page, mensagem) {
+    await expect(page.getByText(mensagem)).toBeVisible();
+  }
